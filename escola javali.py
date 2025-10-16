f9a193a81a43b3b02b1ce053321fe2c2d97877ed
unknown <davidbarros9870@gmail.com>
2025-10-16 11:05:53 +0100

    escola javali.py

---
+++ escola javali.py
@@ -0,0 +1,15 @@
+print("____________________________")
+print(input("Escola javali cansado"))
+print("______________________________")
+n1 = int(input("digite primeira nota : "))
+n2 = int(input("digite a segumda nota : "))
+print("________________________________")
+media =float((n1+n2)/2)
+print("A media e de: {}" . format(media))
+if media >=10 :
+    print ("aluno aprovado")
+    print("__________________________________")
+else: 
+    print("aluno reprovado")
+   
+    print("______________________________")
